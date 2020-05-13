1f4e107b52d59a3838993bca303816c3cf6fc16a
兴达 穆 <597641037@qq.com>
2020-05-13 10:39:08 +0800

    5/13

---
+++ AbstractFactory/AbstractFactory.cpp
@@ -0,0 +1,93 @@
+﻿#include<iostream>
+using namespace std;
+
+//抽象产品类
+class Human
+{
+	//人种接口
+public:
+	virtual void getColor() = 0;
+	virtual void talk() = 0;
+	virtual void getSex() = 0;
+};
+//一次成型
+class AbstractWhiteHuman :public Human
+{
+public:
+	void getColor() {
+		cout << "白色人种的皮肤是白色的" << endl;
+	}
+	void talk() {
+		cout << "白色人种说的话一般都是单字节的" << endl;
+	}
+};
+class AbstractBlackHuman :public Human
+{
+public:
+	void getColor() {
+		cout << "黑色人种的皮肤是黑色的" << endl;
+	}
+	void talk() {
+		cout << "黑色人种说的话一般人听不懂" << endl;
+	}
+};
+class AbstractYellowHuman :public Human
+{
+public:
+	void getColor() {
+		cout << "黄色人种的皮肤是黄色的" << endl;
+	}
+	void talk() {
+		cout << "黄色人种说的话一般是双字节的" << endl;
+	}
+};
+//具体产品
+class FemaleYellowHuman :public AbstractYellowHuman
+{
+public:
+	void getSex() {
+		cout << "黄色人种男性" << endl;
+	}
+};
+class MaleYellowHuman :public AbstractYellowHuman
+{
+public:
+	void getSex() {
+		cout << "黄色人种女性" << endl;
+	}
+};
+//抽象工厂
+class HumanFactory
+{
+public:
+	virtual Human* createYellowHuman() = 0;
+	//virtual Human* createWhiteHuman() = 0;
+	//virtual Human* createBlackHuman() = 0;
+};
+//生产线
+class FemaleFactory :public HumanFactory
+{
+public:
+	Human* createYellowHuman() {
+		return new FemaleYellowHuman();
+	}
+};
+class MaleFactory :public HumanFactory
+{
+public:
+	Human* createYellowHuman() {
+		return new MaleYellowHuman();
+	}
+};
+int main() {
+	HumanFactory* maleHuman = new MaleFactory();
+	HumanFactory* FamaleHuman = new FemaleFactory();
+	Human* YellowHuman = maleHuman->createYellowHuman();
+	YellowHuman->getColor();
+	YellowHuman->getSex();
+	YellowHuman->talk();
+	YellowHuman = FamaleHuman->createYellowHuman();
+	YellowHuman->getColor();
+	YellowHuman->getSex();
+	YellowHuman->talk();
+}
---
+++ TemplateMode/TemplateMode.cpp
@@ -0,0 +1,83 @@
+﻿#include<iostream>
+using namespace std;
+//模板类
+class HummerModel
+{
+public:
+	virtual void start() = 0;
+	virtual void stop() = 0;
+	virtual void alarm() = 0;
+	virtual void engineBoom() = 0;
+	void run() {
+		this->start();
+		this->engineBoom();
+		if (this->isAlarm())
+		{
+			this->alarm();
+		}
+		this->stop();
+	};
+protected:
+	virtual bool isAlarm() {
+		return true;
+	};
+};
+class HummerH1model :public HummerModel
+{
+public:
+	void alarm() {
+		cout << "悍马H1鸣笛" << endl;
+	}
+	void stop() {
+		cout << "悍马H1停车" << endl;
+	}
+	void start() {
+		cout << "悍马H1启动" << endl;
+	}
+	void engineBoom() {
+		cout << "悍马H1引擎声音...." << endl;
+	}
+	void setAlarm(bool isAlarm) {
+		alarFlag = isAlarm;
+	}
+protected:
+	bool isAlarm() {
+		return alarFlag;
+	};
+private:
+	bool alarFlag = true;
+};
+class HummerH2model :public HummerModel
+{
+public:
+	void alarm() {
+		cout << "悍马H2鸣笛" << endl;
+	}
+	void stop() {
+		cout << "悍马H2停车" << endl;
+	}
+	void start() {
+		cout << "悍马H2启动" << endl;
+	}
+	void engineBoom() {
+		cout << "悍马H2引擎声音...." << endl;
+	}
+protected:
+	bool isAlarm() {
+		return false;
+	};
+};
+int main() {
+	while (1)
+	{
+		cout << "悍马H1是否鸣笛？ -- 0 否 1 是		:";
+		bool flag;
+		cin >> flag;
+		HummerModel* H1 = new HummerH1model();
+		((HummerH1model*)H1)->setAlarm(flag);
+		((HummerH1model*)H1)->run();
+		cout << "----------------------------------------" << endl;
+		HummerModel* H2 = new HummerH2model();
+		((HummerH2model*)H2)->run();
+	}
+}
