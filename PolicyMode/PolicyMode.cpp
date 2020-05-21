78678e144c0861ef3a9bc5d7f6bdb3e73c7fc400
兴达 穆 <597641037@qq.com>
2020-05-21 10:11:07 +0800

    5/21

---
+++ PolicyMode/PolicyMode.cpp
@@ -0,0 +1,67 @@
+﻿//-------------------------------------------------
+//策略模式
+//
+//
+//-------------------------------------------------
+#include<iostream>
+
+using namespace std;
+//策略的接口
+class IStrategy
+{
+public:
+	virtual void operate() = 0;
+};
+//具体策略
+class BackDoor :public IStrategy
+{
+public:
+	void operate()
+	{
+		cout << "找乔国老帮忙，让吴国太给孙权施加压力" << endl;
+	}
+};
+class GivenGreeLight :public IStrategy
+{
+public:
+	void operate()
+	{
+		cout << "求吴国太开绿灯，放行。" << endl;
+	}
+};
+class BlockEnemy :public IStrategy
+{
+public:
+	void operate()
+	{
+		cout << "孙夫人断后，挡住追兵。" << endl;
+	}
+};
+//策略分发类
+class Context
+{
+private: 
+	IStrategy* _straegy;
+public:
+	Context(IStrategy* straegy)
+	{
+		this->_straegy = straegy;
+	}
+	void operate()
+	{
+		this->_straegy->operate();
+	}
+};
+int main()
+{
+	Context* context;
+	cout << "--刚到吴国的时候，拆开第一个--" << endl;
+	context = new Context(new BackDoor());
+	context->operate();
+	cout << "--刘备乐不思蜀了，拆开第二个--" << endl;
+	context = new Context(new GivenGreeLight());
+	context->operate();
+	cout << "--孙权的小兵追来了，拆第三个--" << endl;
+	context = new Context(new GivenGreeLight());
+	context->operate();
+}
