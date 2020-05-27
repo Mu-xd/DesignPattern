2a83678629ccc56a85e81f7b6a03359a39901898
兴达 穆 <597641037@qq.com>
2020-05-27 10:21:24 +0800

    05/27

---
+++ FacadeMode/FacadeMode.cpp
@@ -0,0 +1,59 @@
+﻿//---------------------------------------------------------
+//设计模式练习：门面模式
+//注意：一个子系统可以有多个门面、门面不参与子系统内的业务逻辑
+//作者：Mxd--
+//时间：2020/05/27
+//----------------------------------------------------------
+#include<iostream>
+using namespace std;
+//信件子系统接口
+class ILetterProcess
+{
+public:
+	virtual void writeContext(string) = 0;
+	virtual void fillEnvelope(string) = 0;
+	virtual void letterIntoEnvelope() = 0;
+	virtual void sendLetter() = 0;
+};
+class LetterProcessImpl :public ILetterProcess
+{
+public:
+	void writeContext(string context) {
+		cout << "写了一封信...." << context <<endl;
+	};
+	void fillEnvelope(string address) {
+		cout << "写一些地址..."<< address << endl;
+	};
+	void letterIntoEnvelope() {
+		cout << "把信放到信封中...." << endl;
+	};
+	void sendLetter() {
+		cout << "邮递信件...." << endl;
+	};
+};
+//检查子系统
+class Police {
+public:
+	void checkLetter(ILetterProcess* letterprocess) {
+		cout << &letterprocess << "检查完毕！通过" << endl;
+	}
+};
+//门面类
+class ModenPostOffice {
+private:
+	ILetterProcess* _letterprocess = new LetterProcessImpl();
+	Police police;
+public:
+	void sendLetter(string context,string address) {
+		_letterprocess->writeContext(context);
+		_letterprocess->fillEnvelope(address);
+		_letterprocess->letterIntoEnvelope();
+		police.checkLetter(_letterprocess);
+		_letterprocess->sendLetter();
+	}
+};
+int main()
+{
+	ModenPostOffice postOffice;
+	postOffice.sendLetter("----------------------------正文","d地址d");
+}
---
+++ MemoMode/MemoMode.cpp
@@ -0,0 +1,76 @@
+﻿//---------------------------------------------------------
+//设计模式练习：备忘录模式
+//备忘录管理者的引入是为了满足迪米特法则 只和朋友类交流
+//作者：Mxd--
+//时间：2020/05/27
+//----------------------------------------------------------
+#include<iostream>
+using namespace std;
+//备忘录类
+class Memento {
+private:
+	string _state = "";
+public:
+	Memento(string state) {
+		this->_state = state;
+	}
+	string getState()
+	{
+		return this->_state;
+	}
+	void setState(string state)
+	{
+		this->_state = state;
+	}
+};
+class Boy {
+private:
+	string _state = "";
+public:
+	void changeState() {
+		this->_state = "心情可能很不好~";
+	}
+	string getState()
+	{
+		return this->_state;
+	}
+	void setState(string state)
+	{
+		this->_state = state;
+	}
+	Memento createMemento() {
+		return Memento(this->getState());
+	}
+	void readMemento(Memento _memento) {
+		this->setState(_memento.getState());
+	}
+};
+
+class Caretaker {
+private:
+	Memento _memento = Memento("");
+public:
+	Memento getMemento() {
+		return this->_memento;
+	}
+	void setMemento(Memento memento) {
+		this->_memento = memento;
+	}
+};
+int main()
+{
+	Boy boy;
+	Caretaker caretaker;
+	boy.setState("心情很棒");
+	cout << boy.getState() << endl;
+	cout << "-----------------设置标记---------------" << endl;
+	caretaker.setMemento(boy.createMemento());
+	//Memento memento = Memento(boy.getState());
+	cout << "去追女孩子了，失败了" << endl;
+	boy.changeState();
+	cout << boy.getState() << endl;
+	cout << "-----------------恢复心情---------------" << endl;
+	boy.readMemento(caretaker.getMemento());
+	//boy.setState(memento.getState());
+	cout << boy.getState() << endl;
+}
